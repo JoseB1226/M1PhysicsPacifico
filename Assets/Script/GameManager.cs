cf3fbb863126f1c83c6b8dc4f22d5da96d822e0a
JoseB1226 <115236642+JoseB1226@users.noreply.github.com>
2025-01-16 00:06:14 +0800

    gamem1

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -4,27 +4,27 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public int score = 0; // Current score
-    public int lives = 3; // Starting number of lives
-    public TextMesh scoreTextMesh; // Reference to the TextMesh for score
-    public TextMesh livesTextMesh; // Reference to the TextMesh for lives
+    public int score = 0; 
+    public int lives = 3; 
+    public TextMesh scoreTextMesh; 
+    public TextMesh livesTextMesh; 
 
-    // Called when the game starts
+   
     private void Start()
     {
-        // Initialize the TextMesh components with starting values
+        
         UpdateScoreText();
         UpdateLivesText();
     }
 
-    // Called when the player scores
+    
     public void AddScore(int points)
     {
         score += points;
         UpdateScoreText();
     }
 
-    // Called when the player loses a life
+   
     public void LoseLife()
     {
         lives--;
@@ -39,14 +39,14 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    // Triggered when lives run out
+   
     private void GameOver()
     {
         livesTextMesh.text = "Game Over!";
-        scoreTextMesh.text = ""; // Hide score text on game over
+        scoreTextMesh.text = ""; 
     }
 
-    // Updates the score text
+   
     private void UpdateScoreText()
     {
         if (score > 0)
@@ -55,11 +55,11 @@ public class GameManager : MonoBehaviour
         }
         else
         {
-            scoreTextMesh.text = ""; // Hide the score when it's 0
+            scoreTextMesh.text = ""; 
         }
     }
 
-    // Updates the lives text
+   
     private void UpdateLivesText()
     {
         if (lives > 0)
@@ -68,7 +68,7 @@ public class GameManager : MonoBehaviour
         }
         else
         {
-            livesTextMesh.text = ""; // Hide the lives when they're 0
+            livesTextMesh.text = ""; 
         }
     }
 }
